1d89e02a7db3e17c53eafba079a4433e55d5b5ee
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-03-31 02:57:43 -0400

    Navbar

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,17 +1,26 @@
-import { Link } from 'react-router-dom';
-import Navbar from './UI/Navbar';
 
-export default function Nav() {
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {LinkContainer} from 'react-router-bootstrap'
+export default function Navigate() {
   return (
-    <Navbar
-      links={[
-        <Link key={1} className="nav-link text-light" to="/">
-          Home
-        </Link>,
-        <Link key={2} className="nav-link text-light" to="/about">
-          About
-        </Link>,
-      ]}
-    />
+    <Navbar expand="sm" className='navbar-expand-lg bd-navbar fixed-top'>
+      <Container className="container-fluid bg-secondary">
+          <Navbar.Brand className="navbar-brand bg-primary fs-1 text-dark" href='/#'>
+            BRODRIGUEZ<span className="fs-5 text-dark">.dev</span>
+          </Navbar.Brand>
+          <Navbar.Collapse id="navbarSupportedContent">
+            <Nav className="navbar-nav nav-underline me-auto" defaultActiveKey="/">
+              <LinkContainer to="/">
+                <Nav.Link className="text-light" >/HOME</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/about">
+                <Nav.Link className="text-light" >/ABOUT</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+    </Navbar>
   );
 }
