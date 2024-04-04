3ee0d572c580f755e1259a19ec989be8b10fbdd5
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-04 15:26:50 -0400

    seedsjs

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -7,20 +7,8 @@ db.once('open', async () => {
   try {
     await cleanDB('Blog', 'blogs');
 
-
     await Blog.create(blogSeeds);
 
-    // for (let i = 0; i < blogSeeds.length; i++) {
-    //   const { _id, blogTitle } = await Blog.create(blogSeeds[i]);
-    //   const blog = await Blog.findOneAndUpdate(
-    //     {title: blogTitle},
-    //     {
-    //       $addToSet: {
-    //         blogs: _id,
-    //       },
-    //     }
-    //   );
-    // }
   } catch (err) {
     console.error(err);
     process.exit(1);
