bf40b98f64c13abeada07be85eac17989df292d4
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-04 18:09:44 -0400

    Shownone blogs

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,11 +1,11 @@
 import Container from "react-bootstrap/esm/Container";
 
 const Blog = ({blogs = []}) => {
-    // if (!blogs.length) {
-    //     return <>no data</>
-    // }
+    if (!blogs.length) {
+        return <Container as="main">no data</Container>
+    }
 return (
-    <Container className="main text-light">
+    <Container as='main' className="main text-light">
         {blogs && blogs.map((blog) => (<h1 key={blog._id}>{blog.title}</h1>))}
     </Container>
     );
