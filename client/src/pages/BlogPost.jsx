e909456868b0aa82828d7d744f22f2c91959f0de
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-05 16:40:10 -0400

    indiv blog page

---
+++ client/src/pages/BlogPost.jsx
@@ -0,0 +1,20 @@
+import Markdown from 'react-markdown'
+import rehypeRaw from 'rehype-raw'
+import {QUERY_BLOG} from '../utils/queries'
+import { useQuery } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+
+const BlogPost = () => {
+    const {blogId} = useParams();
+    const { loading, data } = useQuery(QUERY_BLOG, {
+        variables: { blogId: blogId },
+      });
+    const blog = data?.blog || [];
+    return (
+        <>
+            <h1>{blog.title}</h1>
+                <Markdown className="text-light" rehypePlugins={[rehypeRaw]}>{blog.content}</Markdown>
+            
+        </>
+)};
+export default BlogPost
