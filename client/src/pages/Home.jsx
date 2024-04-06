a0b5f2bf47939223011306289e5ac41754bcd38c
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-06 17:54:19 -0400

    home gallery

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,17 @@
+import GalleryList from '../components/GalleryList.jsx'
+import {QUERY_GALLERY} from '../utils/queries';
+import { Container } from "react-bootstrap"
+import { useQuery } from '@apollo/client';
+
 export default function Home() {
+  const { loading, data } = useQuery(QUERY_GALLERY)
+    const images = data?.images || [];
     return (
-        <section className="main">
-          <h1>Header 1</h1>
-          <h2>Header 2</h2>
-          <h3>Header 3</h3>
-        </section>
+      <>
+      <h1 className='justify-text-left position-relative border-bottom p-2 m-1'>&gt;/</h1>
+      <Container className="justify-content-lg-left row row-cols-lg-4 row-cols-2 p-0 m-0">
+          <GalleryList className="" images={images} slice="4" />
+      </Container>
+      </>
       );
-    } 
+    };
