e436ac81780267b5805b393f458e078cfef95023
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-06 17:51:57 -0400

    blog tidbit

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -11,14 +11,12 @@ return (
         <Card key={blog._id} className="text-light bg-transparent border border-white border-1 rounded-3 m-2 wrap">
             <Card.Body>
             <Card.Title className="fs-3">    
-                <Link to={`/blog/${blog._id}`} key={blog._id}>{blog.title}</Link>
+                <Link to={`/blog/${blog._id}`} key={blog._id}>{blog.title}</Link> 
             </Card.Title>
-            
-            
-            <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-            </Card.Text>
+            <h6 className="border-bottom border-1"><strong>
+            {blog.date.split('at')[0]}
+            </strong></h6>
+            <Card.Text>{blog.tidbit}</Card.Text>
             </Card.Body>
         </Card>
         </div>))}
