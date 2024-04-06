ac9b4ea9c5c5275c149d3d8e27be499087a34bae
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-06 17:52:25 -0400

    random image return

--- client/src/components/GalleryList.jsx
+++ client/src/components/GalleryList.jsx
@@ -1,37 +1,52 @@
 import { Image, Card, Ratio, Button, Modal } from "react-bootstrap";
 import { useState } from "react";
-const GalleryList = ({images = []}) => {
+var imagesIn = [];
+function shuffle(array){
+            let idx = array.length;
+            let tempArr = Array.from(array);
+            while (idx !=0){
+                console.log('test')
+                let rIdx = Math.floor(Math.random() * idx);
+                var temp = tempArr[idx];
+                tempArr[idx] = tempArr[rIdx];
+                tempArr[rIdx] = temp;
+                idx--;
+            }
+        return(tempArr);
+        };
+const GalleryList = ({images = [], slice = 0}) => {
     if (!images.length) {
         return <>no data</>
     }
     const [show, setShow] = useState(false);
     const [modalData, setModalData] = useState(false);
-
+    if (slice != 0){
+        imagesIn = Array.from(images);
+        imagesIn = shuffle(imagesIn);
+        imagesIn = Array.from(imagesIn.slice(0,slice));
+      }
+      else {imagesIn = Array.from(images)}
 return (
     <>
-        {images && images.map((image, idx) => (
+        {imagesIn && imagesIn.map((image, idx) => (
             <Button key={idx}  className="p-1 m-0 rounded-3" variant="link" 
-            onClick={() => {setShow(true); setModalData(image)}}>
+            onClick={(event) => {event.preventDefault(true); setShow(true); setModalData(image)}}>
                 <Ratio className="rounded-3 shadow-lg" style={{
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                     backgroundImage: `url(${image.path})`}} 
                 aspectRatio="1x1">
-                {/* <Card.Img src="holder.js/100px270" alt="Card image" />
-                    <Card>
-                    <Card.ImgOverlay>
-
-                    </Card.ImgOverlay>
-                    </Card> */}<></>
+                <></>
                 </Ratio>
                 
             </Button>
         ))}
-            <Modal onClick={() => {setShow(false)}} className="text-light modal-lg border-1" centered  show={show} onHide={() => setShow(false)}>
+            <Modal onClick={() => {setShow(false)}} className="text-light modal-md border-1" centered  show={show} onHide={() => setShow(false)}>
                 <Modal.Header className="border-0" >{modalData.title}</Modal.Header>
                 <Image className="rounded-3" src={modalData.path}/>
                    
             </Modal>
+
     </>
     );
 } 
