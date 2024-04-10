8ff37103d3a55c51ae976bb074ed5ecd74bd7879
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-10 03:44:05 -0400

    cleanup & header

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,14 +1,14 @@
 import { useQuery } from '@apollo/client';
 import BlogList from '../components/BlogList'
 import {QUERY_ALL_BLOGS} from '../utils/queries';
-import { Container } from 'react-bootstrap'
+
 const Blog = () => {
     const { loading, data } = useQuery(QUERY_ALL_BLOGS)
     const blogs = data?.blogs || [];
     return (
       <>
-      <h1 className='justify-text-left border-bottom p-2 m-1'>&gt;blog</h1>
-        <div className='row row-cols-1 row-cols-md-2 row-cols-lg-4 p-4 g-2'>
+      <h1 className='justify-text-left border-bottom py-2 m-1 fw-bold'><span className='text-primary'>&gt;</span>blog</h1>
+        <div className='row row-cols-1 row-cols-md-2 row-cols-lg-4 p-1 g-2'>
           <BlogList blogs={blogs}/>
         </div>
         
