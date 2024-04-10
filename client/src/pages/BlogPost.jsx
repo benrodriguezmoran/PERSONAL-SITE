dbefa7fa8b6da0d9c41f1f853b63192af84236eb
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-10 03:44:37 -0400

    blog title

--- client/src/pages/BlogPost.jsx
+++ client/src/pages/BlogPost.jsx
@@ -3,6 +3,7 @@ import rehypeRaw from 'rehype-raw'
 import {QUERY_BLOG} from '../utils/queries'
 import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
+import {LinkContainer} from 'react-router-bootstrap'
 
 const BlogPost = () => {
     const {path} = useParams();
@@ -12,7 +13,7 @@ const BlogPost = () => {
     const blog = data?.blog || [];
     return (
         <>
-            <h1 className="justify-text-left border-bottom p-2 m-1">{blog.title}</h1>
+            <h1 className='justify-text-left border-bottom py-2 m-1 fw-bold '><LinkContainer className='text-primary ' to="/blog"><a className='pe-auto link-primary link-underline-opacity-25 link-underline-opacity-100-hover'>&gt;blog</a></LinkContainer>/<br className='d-lg-none d-flex'></br>{blog.title}</h1>
                 <Markdown className="text-light p-2 m-1" rehypePlugins={[rehypeRaw]}>{blog.content}</Markdown>
             
         </>
