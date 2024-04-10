516d1bb9d6a455e2661ba09ce7ba6e5d349053d2
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-10 03:48:28 -0400

    project seed

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,15 +1,18 @@
 const db = require('../config/connection');
-const { Blog , Image } = require('../models');
+const { Blog , Image ,  Project } = require('../models');
 const blogSeeds = require('./blogSeeds.js');
 const gallerySeeds = require('./gallerySeeds.js')
+const projectSeeds = require('./projectSeeds.js')
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   try {
     await cleanDB('Blog', 'blogs');
-    await cleanDB('Image', 'images')
+    await cleanDB('Image', 'images');
+    await cleanDB('Project', 'projects');
     await Blog.create(blogSeeds);
-    await Image.create(gallerySeeds)
+    await Image.create(gallerySeeds);
+    await Project.create(projectSeeds);
   } catch (err) {
     console.error(err);
     process.exit(1);
