2f7ecc2be1c4ea60bf04bcdc190332239c630ff6
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-10 00:59:39 -0400

    blog breadcrumb

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Card, Ratio } from "react-bootstrap";
 import { Link } from "react-router-dom"
 const Blog = ({blogs = []}) => {
     if (!blogs.length) {
@@ -8,17 +8,21 @@ return (
     <>
         {blogs && blogs.map((blog) => (
         <div className="col" key={blog._id} >
-        <Card className="overflow-hidden text-light bg-transparent border border-white border-1 rounded-3 m-0 wrap ratio ratio-1x1 shadow">
-            <Card.Body>
-            <Card.Title className="fs-3">    
-                <Link to={`/blog/${blog.path}`}>{blog.title}</Link> 
+        <Link to={`/blog/${blog.path}`}>
+        <Ratio aspectRatio={3 / 4}>
+        <Card className="overflow-scroll text-light bg-transparent  border border-white border-1 rounded-3 m-1 wrap shadow" >
+            <Card.Body className="" >
+            <Card.Title className="text-primary link-offset-1 fs-3">    
+                <strong> {blog.title} </strong>
             </Card.Title>
-            <h6 className="border-bottom border-1 position-relative "><strong>
+            <h6 className=" border-bottom border-1"><strong>
             {blog.date.split('at')[0]}
             </strong></h6>
             <Card.Text>{blog.tidbit}</Card.Text>
             </Card.Body>
         </Card>
+        </Ratio>
+        </Link>
         </div>))}
     </>
     );
