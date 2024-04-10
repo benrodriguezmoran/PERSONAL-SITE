439cfd3b84e151649a402e397fe48a9e38677bf7
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-10 01:02:53 -0400

    cleanup

--- client/src/pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-        <main className="main">
-          <h1>Header 1</h1>
-          <h2>Header 2</h2>
-          <h3>Header 3</h3>
-        </main>
-      );
-    } 
