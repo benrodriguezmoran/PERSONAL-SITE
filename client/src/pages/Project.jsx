1632399c99376610d9fda85ef4a1051710b9c188
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-10 18:57:52 -0400

    styling

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -18,7 +18,7 @@ const Project = () => {
     const projectImg = project.image || [];
     return (
         <>  
-        <h1 className='justify-text-left border-bottom py-2 m-1 fw-bold '><LinkContainer className='text-primary ' to="/portfolio"><a className='pe-auto link-primary link-underline-opacity-25 link-underline-opacity-100-hover'>&gt;portfolio</a></LinkContainer>/<br className='d-xl-none d-flex'></br>{project.title}</h1>
+        <h1 className='justify-text-left border-bottom py-2 m-1 fw-bold '><LinkContainer className='text-primary ' to="/portfolio"><a className='pe-auto link-primary link-underline-opacity-25 link-underline-opacity-100-hover'>&gt;portfolio</a></LinkContainer><br className='d-xl-none d-flex'></br>/{project.title}</h1>
              <Ratio onClick={(event) => {event.preventDefault(true); setShow(true); setModalData(projectImg)}} className="rounded-3 shadow-lg" style={{
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
