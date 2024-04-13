e39198b5b08d3ec26519f895dc46bbf2ac30bb06
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-13 13:24:01 -0400

    site showcase

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import GalleryList from '../components/GalleryList.jsx'
 import { Container, Row, Col, Card, Ratio } from "react-bootstrap"
-import { about1, about2, about3 } from './content/homeContent.js'
 import { Link } from "react-router-dom"
 import Markdown from 'react-markdown'
 import useBreakpoint from 'bootstrap-5-breakpoint-react-hook';
+import { about1, about2, about3 } from './content/homeContent.js'
 import rehypeRaw from 'rehype-raw'
 import { useQuery } from '@apollo/client';
 import {QUERY_PROJECT, QUERY_GALLERY} from '../utils/queries'
@@ -12,8 +12,12 @@ export default function Home() {
   const projectData = useQuery(QUERY_PROJECT, {
         variables: { path : "spacegame" },
       });
+  const siteData = useQuery(QUERY_PROJECT, {
+        variables: { path : "site" },
+      });    
   const galleryData = useQuery(QUERY_GALLERY);
   var project = projectData.data?.project;
+  var site = siteData.data?.project;
   const images = galleryData.data?.images || [];
   const loading = galleryData.loading || projectData.loading;
 
@@ -47,57 +51,78 @@ export default function Home() {
             <GalleryList className="" images={images} slice={slice} />
         </Container>
         </Row>
-          <Row className='row-cols-lg-2 row-cols-1 py-3 my-3 border-bottom d-flex' >
-            <Col className='text-light lh-lg d-flex order-lg-1  align-items-center'>
-              <div>
-                <Markdown rehypePlugins={[rehypeRaw]}>
-                  {about2()}
-                </Markdown>
-                <Link className='col p-0 link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover' to="/portfolio/spacegame">
-                  here</Link>, or my other projects on my <Link className='col p-0 link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover' to="/portfolio">portfolio</Link>.
-              </div> 
-
-            </Col>
-            <Link className='col p-1 pb-3 order-lg-0 py-3' style={{textDecoration: "none"}} to={`/portfolio/spacegame`}>
-                <Card className=" text-light bg-tertiary bg-gradient border-white border-1 rounded-3 m-0 p-0 wrap shadow" >
-                  <Ratio aspectRatio={1/2}>
-                    <Card.Img variant="top" className="m-0 p-0" style={{ 
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center', 
-                      backgroundImage: `url(${project.image})`}} />
-                  </Ratio>
-                  <Card.Body className="p-0 m-0" >
-                    <Container className="p-3" >
-                      <Card.Title className="text-primary fs-3 border-bottom border-1">    
-                            <strong>{project.title}</strong>
-                      </Card.Title>
-  
-                      <Card.Text>{project.tidbit}</Card.Text>
-                    </Container>
-                  </Card.Body>
-                </Card>
-            </Link>
-          </Row>
-        <Row className='p-0 row-cols-lg-2 row-cols-1 py-3 my-3 border-bottom d-flex align-items-center' > 
-            <Col className='text-light lh-lg'>
-              <div>
+        <Row className='row-cols-lg-2 row-cols-1 py-3 my-3 border-bottom d-flex' >
+          <Col className='text-light lh-lg d-flex order-lg-1  align-items-center'>
+            <div>
               <Markdown rehypePlugins={[rehypeRaw]}>
-                {about3()}
-              </Markdown> Feel free to <Link className='col p-0 link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover' to="/gallery">contact me
-              </Link> to inquire reguarding software or web design opportunities.
-              </div>
-            </Col>
+                {about2()}
+              </Markdown>
+              Explore my projects at my <Link className='col p-0 link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover' to="/portfolio">portfolio</Link>.
+            </div> 
+
+          </Col>
+          <Link className='col p-1 pb-3 order-lg-0 py-3' style={{textDecoration: "none"}} to={`/portfolio/spacegame`}>
+              <Card className=" text-light bg-tertiary bg-gradient border-white border-1 rounded-3 m-0 p-0 wrap shadow" >
+                <Ratio aspectRatio={1/2}>
+                  <Card.Img variant="top" className="m-0 p-0" style={{ 
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center', 
+                    backgroundImage: `url(${project.image})`}} />
+                </Ratio>
+                <Card.Body className="p-0 m-0" >
+                  <Container className="p-3" >
+                    <Card.Title className="text-primary fs-3 border-bottom border-1">    
+                          <strong>{project.title}</strong>
+                    </Card.Title>
+
+                    <Card.Text>{project.tidbit}</Card.Text>
+                  </Container>
+                </Card.Body>
+              </Card>
+          </Link>
+        </Row>
+        <Row className='p-0 row-cols-lg-2 row-cols-1 py-3 my-3 border-bottom d-flex align-items-center' > 
+          <Col className='text-light lh-lg'>
+            <div>
+            <Markdown rehypePlugins={[rehypeRaw]}>
+              {about3()}
+            </Markdown>
+            </div>
+          </Col>
+          <Link className='col p-1 pb-3 order-lg-0 py-3' style={{textDecoration: "none"}} to={`/portfolio/spacegame`}>
+              <Card className=" text-light bg-tertiary bg-gradient border-white border-1 rounded-3 m-0 p-0 wrap shadow" >
+                <Ratio aspectRatio={1/2}>
+                  <Card.Img variant="top" className="m-0 p-0" style={{ 
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center', 
+                    backgroundImage: `url(${site.image})`}} />
+                </Ratio>
+                <Card.Body className="p-0 m-0" >
+                  <Container className="p-3" >
+                    <Card.Title className="text-primary fs-3 border-bottom border-1">    
+                          <strong>{site.title}</strong>
+                    </Card.Title>
+
+                    <Card.Text>{site.tidbit}</Card.Text>
+                  </Container>
+                </Card.Body>
+              </Card>
+          </Link>
+        </Row>
+        <Row className='p-0 row-cols-lg-2 row-cols-1 py-3 my-3 border-bottom d-flex align-items-center' >
           <Col className='p-1 py-3' >
             <Ratio className="rounded-3 shadow-lg " style={{
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                     backgroundImage: 'url(/images/certificate.png)'}} 
                 aspectRatio={1/1.7}>
-                <></>
-              </Ratio>
-            </Col>
+              <></>
+            </Ratio>
+          </Col>
         </Row>
       </Container>
       </>
       );
     };
+
+    
