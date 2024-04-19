ee35e5aa1802913edc55b572bfecee0e8be10bd4
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-19 11:54:08 -0400

    placeholder

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -7,9 +7,9 @@ const Gallery = () => {
     const images = data?.images || [];
     const rows = [];
     console.log(loading)
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < 25; i++) {
       rows.push(<div key={i} >
-      <Ratio className="image rounded-3 shadow-lg p-1 m-0 bg-secondary rounded-3 placeholder-wave" aspectRatio="1x1">
+      <Ratio className="image rounded-3 shadow-lg p-0 m-1 bg-secondary rounded-3 placeholder-wave" aspectRatio="1x1">
       <></>
       </Ratio>
       </div>);
