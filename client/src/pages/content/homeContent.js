efa39c2b5f50c0db793ac0b89c44ef321450e58f
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-04-24 21:04:07 -0400

    content

--- client/src/pages/content/homeContent.js
+++ client/src/pages/content/homeContent.js
@@ -7,21 +7,19 @@ const str = `
 <span className="px-3"></span>I am a full-stack web developer and aspiring game dev from the beautiful state of Michigan.
 I'm technical by nature; if there's something to be learned, I'll be sure to have it understood. 
 To the side you'll see a random set of pictures I've taken and want to share. You'll find my scenic photos, family's pets, projects, and more 
-
 `
 const str2 = `
 <div className="text-center border-bottom mb-2">\n
 #### <strong>I build things</strong>
 </div>
-<span className="px-3"></span>I am a huge 3d printing enthusiast, own 3 printers, and one of my jobs involved maintaining a fleet. Most of my other hobbies revolve around problem solving: be it programming, electronics, or mechanical design. I do these things for a kind of entertainment, some satisfaction of finally putting a challenge to rest. 
+<span className="px-3"></span>I am a long-time 3d printing enthusiast, own 3 printers, and one of my jobs involved maintaining a fleet. I'm developing a game in Godot, something I hope to share in time. 
+I'm facinated by control systems, since a rocketry challenge in highschool I've iterated on my own flight control code, eventually to publish an open source drone. 
 `
 const str3 = `
 <div className="text-center border-bottom mb-2">\n
-#### <strong>I built this page</strong>
+#### <strong>I build websites</strong>
 </div>
-<span className="px-3"></span>You can only learn what you don't know through necessity, so I learned everything to host this page. Certainly a lot more than necessary, but it gives me  
-a practical demonstration of my knowledge, and a place to dedicate my personal work to and advance my software career.
-
+<span className="px-3"></span>Including this one. It gives me a place to dedicate my personal work to 
 `
 export function about1() {
     return str;
